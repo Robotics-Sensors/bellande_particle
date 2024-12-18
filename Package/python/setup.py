26bbcd7cbbc097c7f4e2213fe0946b1465eb913d
RonaldsonBellande <ronaldsonbellande@gmail.com>
2024-12-18 10:09:38 -0500

    latest pushes

--- Package/python/setup.py
+++ Package/python/setup.py
@@ -5,7 +5,7 @@ with open("./README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bellande_particle",
-    version="0.0.1",
+    version="0.0.2",
     description="Robotics Particle",
     long_description=long_description,
     long_description_content_type="text/markdown",
